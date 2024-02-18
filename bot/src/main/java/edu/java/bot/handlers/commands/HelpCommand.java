178eed49575056957d3e1b13815cd181dbd03723
MIZINCHIK <mizinchik@yahoo.com>
2024-02-19 02:04:09 +0700

    Commands: Create classes for dealing with certain bot commands

---
+++ bot/src/main/java/edu/java/bot/handlers/commands/HelpCommand.java
@@ -0,0 +1,32 @@
+package edu.java.bot.handlers.commands;
+
+import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.model.Message;
+import edu.java.bot.PrimaveraBot;
+import net.steppschuh.markdowngenerator.table.Table;
+import net.steppschuh.markdowngenerator.text.code.CodeBlock;
+
+public class HelpCommand extends Command {
+    private static final String NAME = "help";
+    private static final String DESCRIPTION = "Show commands. No arguments.";
+
+    public HelpCommand() {
+        super(NAME, DESCRIPTION);
+    }
+
+    private String formCommands(PrimaveraBot bot) {
+        BotCommand[] commands = bot.getCommands();
+        Table.Builder tableBuilder = new Table.Builder()
+            .withAlignments(Table.ALIGN_CENTER, Table.ALIGN_CENTER)
+            .addRow("Command", "Description");
+        for (BotCommand command : commands) {
+            tableBuilder.addRow(command.command(), command.description());
+        }
+        return new CodeBlock(tableBuilder.build().toString(), "Commands").toString();
+    }
+
+    public void handle(Message message, String[] args, PrimaveraBot bot) {
+        String commands = formCommands(bot);
+        bot.respondMd(message.chat().id(), message.messageId(), commands);
+    }
+}
---
+++ bot/src/main/java/edu/java/bot/handlers/commands/ListCommand.java
@@ -0,0 +1,40 @@
+package edu.java.bot.handlers.commands;
+
+import com.pengrad.telegrambot.model.Message;
+import edu.java.bot.PrimaveraBot;
+import edu.java.bot.links.Link;
+import net.steppschuh.markdowngenerator.table.Table;
+import net.steppschuh.markdowngenerator.text.code.CodeBlock;
+import java.net.URL;
+import java.util.List;
+
+public class ListCommand extends Command {
+    private static final String NAME = "list";
+    private static final String DESCRIPTION = "List the links being tracked. No arguments.";
+    private static final String EMPTY_TRACKING_LIST = "No links being tracked.";
+
+    public ListCommand() {
+        super(NAME, DESCRIPTION);
+    }
+
+    @Override
+    public void handle(Message message, String[] args, PrimaveraBot bot) {
+        if (!bot.isUserRegistered(message.from().id())) {
+            handleUserNotRegistered(message, bot);
+        } else {
+            List<URL> tracked = bot.getLinksByUserId(message.from().id());
+            if (tracked.isEmpty()) {
+                bot.respond(message.chat().id(), message.messageId(), EMPTY_TRACKING_LIST);
+            } else {
+                Table.Builder tableBuilder = new Table.Builder()
+                    .withAlignments(Table.ALIGN_CENTER, Table.ALIGN_CENTER)
+                    .addRow("Website", "Link");
+                tracked.stream()
+                    .map(link -> new Link(link.toString()))
+                    .forEach(link -> tableBuilder.addRow(link.getDomain(), link.getUrl().toString()));
+                bot.respondMd(message.chat().id(), message.messageId(),
+                    new CodeBlock(tableBuilder.build().toString(), "Tracked links").toString());
+            }
+        }
+    }
+}
