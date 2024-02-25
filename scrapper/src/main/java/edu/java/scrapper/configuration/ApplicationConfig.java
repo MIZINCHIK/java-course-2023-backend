8be2f4ca006fc7d0c0fc80e409b6c8e88af315b4
MIZINCHIK <mizinchik@yahoo.com>
2024-02-25 21:38:26 +0700

    Configuration: Create configs for external services

---
+++ scrapper/src/main/java/edu/java/scrapper/configuration/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package edu.java.scrapper.configuration;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.Duration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+public record ApplicationConfig(
+    @NotNull
+    Scheduler scheduler,
+    Github github,
+    StackOverflow stackOverflow
+) {
+    public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
+    }
+
+    public record Github(String baseUrl) {
+    }
+
+    public record StackOverflow(String baseUrl) {
+    }
+}
