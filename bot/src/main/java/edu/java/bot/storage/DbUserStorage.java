f195b8453e6c865b6c4cdfc0275d2dc26f827d0e
MIZINCHIK <mizinchik@yahoo.com>
2024-02-26 10:58:27 +0700

    Fixe Storage: Divide into user and links classes

---
+++ bot/src/main/java/edu/java/bot/storage/DbUserStorage.java
@@ -0,0 +1,16 @@
+package edu.java.bot.storage;
+
+public class DbUserStorage implements UserStorage {
+
+    @Override
+    public void registerUser(Long userId) {
+        //TODO
+    }
+
+    @Override
+    public boolean isUserRegistered(Long userId) {
+        // TODO
+        return true;
+    }
+
+}
