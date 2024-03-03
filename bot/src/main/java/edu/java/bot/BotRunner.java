bde9d796368c7cc4b902e44955ccb1db7926d460
MIZINCHIK <mizinchik@yahoo.com>
2024-03-04 03:17:54 +0700

    General: Fixes & preparations

--- bot/src/main/java/edu/java/bot/BotRunner.java
+++ bot/src/main/java/edu/java/bot/BotRunner.java
@@ -1,6 +1,5 @@
 package edu.java.bot;
 
-import edu.java.bot.configuration.ApplicationConfig;
 import edu.java.bot.handlers.BotExceptionHandler;
 import edu.java.bot.handlers.UpdateHandler;
 import edu.java.bot.handlers.UpdateHandlerBuilder;
@@ -10,33 +9,23 @@ import edu.java.bot.handlers.commands.StartCommand;
 import edu.java.bot.handlers.commands.TrackCommand;
 import edu.java.bot.handlers.commands.UnknownCommand;
 import edu.java.bot.handlers.commands.UntrackCommand;
-import edu.java.bot.storage.DbLinkStorage;
-import edu.java.bot.storage.DbUserStorage;
-import edu.java.bot.storage.LinkStorage;
-import edu.java.bot.storage.UserStorage;
-import org.springframework.beans.factory.annotation.Autowired;
+import edu.java.model.storage.LinkStorage;
+import edu.java.model.storage.UserStorage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class BotRunner implements ApplicationRunner {
-    private final String token;
-    private PrimaveraBot bot;
-    private UserStorage userStorage;
-    private LinkStorage linkStorage;
+    private final PrimaveraBot bot;
+    private final UserStorage userStorage;
+    private final LinkStorage linkStorage;
     private UpdateHandler handler;
-
-    @Autowired
-    public BotRunner(ApplicationConfig applicationConfig) {
-        token = applicationConfig.telegramToken();
-    }
 
     @Override
     public void run(ApplicationArguments args) {
-        bot = new PrimaveraBot(token);
-        userStorage = new DbUserStorage();
-        linkStorage = new DbLinkStorage();
         configureHandler();
         bot.setCommands(handler.getCommands());
         bot.setUpdatesListener(new BotUpdatesListener(handler), new BotExceptionHandler());
