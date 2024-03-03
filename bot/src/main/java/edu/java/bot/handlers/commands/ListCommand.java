caeac3fd308bb975633d5985af1265b8c9b6c591
MIZINCHIK <mizinchik@yahoo.com>
2024-03-04 03:25:25 +0700

    Bot: Fix storage

--- bot/src/main/java/edu/java/bot/handlers/commands/ListCommand.java
+++ bot/src/main/java/edu/java/bot/handlers/commands/ListCommand.java
@@ -2,9 +2,10 @@ package edu.java.bot.handlers.commands;
 
 import com.pengrad.telegrambot.model.Message;
 import edu.java.bot.PrimaveraBot;
-import edu.java.bot.storage.LinkStorage;
-import edu.java.bot.storage.UserStorage;
-import java.net.URL;
+import edu.java.model.dto.LinkResponse;
+import edu.java.model.storage.LinkStorage;
+import edu.java.model.storage.UserStorage;
+import java.net.URI;
 import java.util.List;
 import static edu.java.bot.handlers.MessageFormatter.buildLinks;
 
@@ -26,7 +27,8 @@ public class ListCommand extends Command {
         if (!userStorage.isUserRegistered(message.from().id())) {
             handleUserNotRegistered(message);
         } else {
-            List<URL> tracked = linkStorage.getLinksByUserId(message.from().id());
+            List<URI> tracked =
+                linkStorage.getLinksByUserId(message.from().id()).stream().map(LinkResponse::url).toList();
             if (tracked.isEmpty()) {
                 bot.respond(message.chat().id(), message.messageId(), EMPTY_TRACKING_LIST);
             } else {
