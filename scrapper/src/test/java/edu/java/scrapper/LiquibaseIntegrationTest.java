0ae3ba177466a9ceff91925e3ab239fbabecd240
MIZINCHIK <mizinchik@yahoo.com>
2024-03-10 15:26:26 +0700

    Scrapper: Implement basic integration test

---
+++ scrapper/src/test/java/edu/java/scrapper/LiquibaseIntegrationTest.java
@@ -0,0 +1,11 @@
+package edu.java.scrapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LiquibaseIntegrationTest extends IntegrationTest {
+    @Test
+    void test() {
+        Assertions.assertTrue(POSTGRES.isRunning());
+    }
+}
