e4709d8076c55900fd7ceb85935d3f9dcb5c08b9
MIZINCHIK <mizinchik@yahoo.com>
2024-03-31 15:55:47 +0700

    General: Fix codestyle

--- scrapper/src/main/java/edu/java/scrapper/updates/LinkUpdater.java
+++ scrapper/src/main/java/edu/java/scrapper/updates/LinkUpdater.java
@@ -15,7 +15,6 @@ import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
