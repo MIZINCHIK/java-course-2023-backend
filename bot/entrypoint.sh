af0d19c780969faa3fa725f0130c54d9da0adeb1
MIZINCHIK <mizinchik@yahoo.com>
2024-04-21 17:04:51 +0700

    Docker Compose: Add secret API key & fix Dockerfiles

--- bot/entrypoint.sh
+++ bot/entrypoint.sh
@@ -1,24 +1,2 @@
-#!/usr/bin/env bash
-
-set -e
-
-file_env() {
-   local var="$1"
-   local fileVar="${var}_FILE"
-   local def="${2:-}"
-
-   if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
-      echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
-      exit 1
-   fi
-   local val="$def"
-   if [ "${!var:-}" ]; then
-      val="${!var}"
-   elif [ "${!fileVar:-}" ]; then
-      val="$(< "${!fileVar}")"
-   fi
-   export "$var"="$val"
-   unset "$fileVar"
-}
-
-file_env "APP_TELEGRAM_TOKEN"
+#!/bin/sh
+export APP_TELEGRAM_TOKEN=$(cat /run/secrets/tg_api_key) ; java -jar /bot.jar
